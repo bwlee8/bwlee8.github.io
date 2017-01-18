f66434d6ebcacb550118578d1e3614180e803880
Brian Lee <bwlee8@gmail.com>
2017-01-17 19:11:18 -0500

    added if else statements

--- homework/lifetime-supply/js/project.js
+++ homework/lifetime-supply/js/project.js
@@ -3,16 +3,37 @@ document.getElementById('click-me').onclick = userInput;
 
 function userInput(){
 	var currentAge = document.getElementById('age').value;
-		console.log(currentAge);
-
+		if (currentAge < 0 || isNaN(currentAge) == true){
+			console.log('current age = '+ currentAge + ' ' +isNaN(currentAge));
+			return false;
+		}
+		else{
+			console.log(currentAge);
+		}
 	var maxAge = document.getElementById('max-age').value;
-		console.log(maxAge);
+		if (maxAge < 0|| isNaN(maxAge) == true){
+			console.log('max age = '+ maxAge + ' ' + isNaN(maxAge));
+			return false;
+		}
+		else{
+			console.log(maxAge);
+		}
+
+
 
 	var favDrink = document.getElementById('item').value;
 		console.log(favDrink);
 
 	var numDrink = document.getElementById('num-per-day').value;
-		console.log(numDrink);
+
+		if (numDrink < 0 || isNaN(numDrink) == true){
+			console.log('number of drinks = ' + numDrink + ' ' + isNaN(numDrink));
+			return false;
+		}
+
+		else{
+			console.log(numDrink);
+		}
 
 	var totalYearsLeft = maxAge - currentAge;
 		console.log(totalYearsLeft);
