32c32b217b23c01c015c8e8483567539daaaa417
Brian Lee <bwlee8@gmail.com>
2017-02-20 22:11:26 -0500

    added more images

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -1,29 +1,24 @@
-$(document).ready(function(){
-	$('.preview_img').click(function(event){
-		event.preventDefault();
-		event.stopPropagation();
-		console.log('clicked!')
+$(document).ready(function() {
+    $('.preview_img').click(function(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        console.log('clicked!')
 
-		$('.bigimage_wrapper').removeClass('bigimage_wrapper_hidden');
+        $('.bigimage_wrapper').removeClass('bigimage_wrapper_hidden');
 
-		var image = $(this).attr('src');
+        var image = $(this).attr('src');
 
-		var prevSrc = $(this).parents('.photoGrid').prev().find('.preview_img').attr('src');
+        var prevSrc = $(this).parents('.photoGrid').prev().find('.preview_img').attr('src');
 
-		// .prev().find('.preview_img').attr();
-		
-		console.log(prevSrc);
+        // .prev().find('.preview_img').attr();
 
-		//changes big image to 'img/2.jpg' or 'img/3.jpg', etc
-		$('.bigimage').attr('src', image);
-	})	
+        console.log(prevSrc);
 
-	$('.bigimage_wrapper').click(function(){
-		$('.bigimage_wrapper').addClass('bigimage_wrapper_hidden');
-	})
+        //changes big image to 'img/2.jpg' or 'img/3.jpg', etc
+        $('.bigimage').attr('src', image);
+    })
 
-	// function returnToOriginalSize() {
- //    	$(this).css({height: "", width: ""});
- //    }
-
+    $('.bigimage_wrapper').click(function() {
+        $('.bigimage_wrapper').addClass('bigimage_wrapper_hidden');
+    })
 })
