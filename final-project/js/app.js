8fdc1a06f28e68ab65a395ce8b9facc7cef75660
Brian Lee <bwlee8@gmail.com>
2017-02-22 17:23:19 -0500

    added JS to filter in final project

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -21,4 +21,70 @@ $(document).ready(function() {
     $('.bigimage_wrapper').click(function() {
         $('.bigimage_wrapper').addClass('bigimage_wrapper_hidden');
     })
+
+    $('.filter_all').click(function(event){
+        event.preventDefault();
+        console.log('NA Filter!');
+        $('.asia').show(200)
+        $('.north_america').show(200);
+        $('.south_america').show(200);
+        $('.europe').show(200);
+        $('.oceania').show(200);
+    })
+
+    $('.filter_NA').click(function(event){
+        event.preventDefault();
+        console.log('NA Filter!');
+        $('.north_america').css('display','inline-block');
+        $('.asia').css('display','none');
+        $('.south_america').css('display','none');
+        $('.europe').css('display','none');
+        $('.oceania').css('display','none');
+    })
+
+    $('.filter_SA').click(function(event){
+        event.preventDefault();
+        console.log('SA Filter!');
+        $('.south_america').css('display','inline-block');
+        $('.asia').css('display','none');
+        $('.north_america').css('display','none');
+        $('.europe').css('display','none');
+        $('.oceania').css('display','none');
+    })
+    $('.filter_EU').click(function(event){
+        event.preventDefault();
+        console.log('EU Filter!');
+        $('.europe').css('display','inline-block');
+        $('.asia').css('display','none');
+        $('.north_america').css('display','none');
+        $('.south_america').css('display','none');
+        $('.oceania').css('display','none');
+    })
+      $('.filter_OC').click(function(event){
+        event.preventDefault();
+        console.log('OC Filter!');
+        $('.oceania').css('display','inline-block');
+        $('.asia').css('display','none');
+        $('.north_america').css('display','none');
+        $('.south_america').css('display','none');
+        $('.europe').css('display','none');
+    })
+       $('.filter_AZ').click(function(event){
+        event.preventDefault();
+        console.log('AZ Filter!');
+        $('.asia').show()
+        $('.ocenia').hide(400);
+        $('.south_america').hide(400);
+        $('.north_america').hide(400);
+        $('.europe').hide(400);
+       
+        // $('.oceania').animate({display: 'none'});
+        //  $('.north_america').animate({display: 'none'});
+        //   $('.south_america').animate({display: 'none'});
+        //    $('.europe').animate({display: 'none'});
+        // $('.oceania').css('display','none');
+        // $('.north_america').css('display','none');
+        // $('.south_america').css('display','none');
+        // $('.europe').css('display','none');
+    })
 })
