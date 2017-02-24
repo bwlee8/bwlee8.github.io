72faa448d9657dd5b11a617be30fc0fa5bc85d9c
Brian Lee <bwlee8@gmail.com>
2017-02-23 19:13:58 -0500

    Prettify code

--- final-project/js/app.js
+++ final-project/js/app.js
@@ -1,105 +1,104 @@
 $(document).ready(function() {
 
-  //   var $window = $(window);
-  //   var isFixed = false;
-  //   var init = $window.offset().Top();
+    //   var $window = $(window);
+    //   var isFixed = false;
+    //   var init = $window.offset().Top();
 
-  // $window.scroll(function() {
-  //   var currentScrollTop = $window.scrollTop();
-  //   if (currentScrollTop > init && isFixed === false) {
-  //     isFixed = true;
-  //    $('header').addClass('header_offset');
-  //    $('.filter').addClass('filter_offset');
-  //    $('.filter_option').addClass('filter_option_offset');
-  //     console.log("fixed");
-  //   } else if (currentScrollTop <= init && isFixed === true) {
-  //     isFixed = false;
-  //     $('header').removeClass('header_offset');
-  //     $('.filter').removeClass('filter_offset');
-  //     $('.filter_option').removeClass('filter_option_offset');
-  //     console.log("unfixed");
-  //   }
-  // });
-$(window).resize(function () {
+    // $window.scroll(function() {
+    //   var currentScrollTop = $window.scrollTop();
+    //   if (currentScrollTop > init && isFixed === false) {
+    //     isFixed = true;
+    //    $('header').addClass('header_offset');
+    //    $('.filter').addClass('filter_offset');
+    //    $('.filter_option').addClass('filter_option_offset');
+    //     console.log("fixed");
+    //   } else if (currentScrollTop <= init && isFixed === true) {
+    //     isFixed = false;
+    //     $('header').removeClass('header_offset');
+    //     $('.filter').removeClass('filter_offset');
+    //     $('.filter_option').removeClass('filter_option_offset');
+    //     console.log("unfixed");
+    //   }
+    // });
+    $(window).resize(function() {
         console.log($(window).width())
-    
-        if ($(window).width() > 880){
+
+        if ($(window).width() > 880) {
             $('.small_nav').hide();
         }
 
     })
 
-    $('.burger').click(function(){
+    $('.burger').click(function() {
         $('.small_nav').slideToggle();
     })
 
     //timer for scroll function. purpose to reduce lag of scroll function
-var scrollTimer = null;
-$(window).scroll(function () {
+    var scrollTimer = null;
+    $(window).scroll(function() {
         if (scrollTimer) {
-            clearTimeout(scrollTimer);   // clear any previous pending timer
+            clearTimeout(scrollTimer); // clear any previous pending timer
         }
-        scrollTimer = setTimeout(handleScroll, 100);   // set new timer
+        scrollTimer = setTimeout(handleScroll, 100); // set new timer
     });
 
-//function that shortens height of header/filter when scrolling down
-//jQuery collections are cached with boolean statements
-function handleScroll(){
-    var shrink = false;
-    var init = $('.subheader').offset().top;
+    //function that shortens height of header/filter when scrolling down
+    //jQuery collections are cached with boolean statements
+    function handleScroll() {
+        var shrink = false;
+        var init = $('.subheader').offset().top;
 
-    $(window).scroll(function(){
-    
-        var offset = $(window).scrollTop();
+        $(window).scroll(function() {
 
-        if (offset > init && shrink === false){
-            shrink = true;
-            $('header').addClass('header_offset');
-            $('.filter').addClass('filter_offset');
-            $('.filter_option').addClass('filter_option_offset');
-            console.log('shrunk!');
-        }
-        else if (offset <= init && shrink === true){
-            shrink = false;
-            $('header').removeClass('header_offset');
-            $('.filter').removeClass('filter_offset');
-            $('.filter_option').removeClass('filter_option_offset');
-            console.log('expanded!');
-        }
-        console.log('scrolling');
+            var offset = $(window).scrollTop();
 
-    })
-}
+            if (offset > init && shrink === false) {
+                shrink = true;
+                $('header').addClass('header_offset');
+                $('.filter').addClass('filter_offset');
+                $('.filter_option').addClass('filter_option_offset');
+                console.log('shrunk!');
+            } else if (offset <= init && shrink === true) {
+                shrink = false;
+                $('header').removeClass('header_offset');
+                $('.filter').removeClass('filter_offset');
+                $('.filter_option').removeClass('filter_option_offset');
+                console.log('expanded!');
+            }
+            console.log('scrolling');
 
-//clicked image is enlarged and center in the page. background is dimmed. position of enlarged image and background is fixed and takes up entire screen
+        })
+    }
+
+    //clicked image is enlarged and center in the page. background is dimmed. position of enlarged image and background is fixed and takes up entire screen
     $('.preview_img').click(function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        console.log('clicked!')
+            event.preventDefault();
+            event.stopPropagation();
+            console.log('clicked!')
 
-        $('.bigimage_wrapper').removeClass('bigimage_wrapper_hidden');
+            $('.bigimage_wrapper').removeClass('bigimage_wrapper_hidden');
 
-        var image = $(this).attr('src');
+            var image = $(this).attr('src');
 
-        var prevSrc = $(this).parents('.photoGrid').prev().find('.preview_img').attr('src');
+            var prevSrc = $(this).parents('.photoGrid').prev().find('.preview_img').attr('src');
 
-        // .prev().find('.preview_img').attr();
+            // .prev().find('.preview_img').attr();
 
-        console.log(prevSrc);
+            console.log(prevSrc);
 
-        //changes big image to 'img/2.jpg' or 'img/3.jpg', etc
-        $('.bigimage').attr('src', image);
-    })
-//adds bigimage_wrapper class. the background for the enlarged image
+            //changes big image to 'img/2.jpg' or 'img/3.jpg', etc
+            $('.bigimage').attr('src', image);
+        })
+        //adds bigimage_wrapper class. the background for the enlarged image
     $('.bigimage_wrapper').click(function() {
         $('.bigimage_wrapper').addClass('bigimage_wrapper_hidden');
     })
 
-//filter function
-    $('.filter_all').click(function(event){
+    //filter function
+    $('.filter_all').click(function(event) {
         event.preventDefault();
         console.log('NA Filter!');
-        $(this).parent().siblings().removeClass('filter_active');   
+        $(this).parent().siblings().removeClass('filter_active');
         $('.asia').fadeIn(700)
         $('.north_america').fadeIn(700);
         $('.south_america').fadeIn(700);
@@ -107,8 +106,8 @@ function handleScroll(){
         $('.oceania').fadeIn(700);
     })
 
-//filters out photos not in North America
-  $('.filter_NA').click(function(event){
+    //filters out photos not in North America
+    $('.filter_NA').click(function(event) {
         event.preventDefault();
         $(this).parent().addClass('filter_active').siblings().removeClass('filter_active');
 
@@ -120,8 +119,8 @@ function handleScroll(){
         $('.europe').fadeOut(500);
     })
 
-//filters out photos not in South America
-    $('.filter_SA').click(function(event){
+    //filters out photos not in South America
+    $('.filter_SA').click(function(event) {
         event.preventDefault();
         $(this).parent().addClass('filter_active').siblings().removeClass('filter_active');
         console.log('SA Filter!');
@@ -132,8 +131,8 @@ function handleScroll(){
         $('.europe').fadeOut(500);
     })
 
-//filters out photos not in Europe
-    $('.filter_EU').click(function(event){
+    //filters out photos not in Europe
+    $('.filter_EU').click(function(event) {
         event.preventDefault();
         $(this).parent().addClass('filter_active').siblings().removeClass('filter_active');
         console.log('EU Filter!');
@@ -144,8 +143,8 @@ function handleScroll(){
         $('.asia').fadeOut(500);
     })
 
-//filters out photos not in Oceania
-      $('.filter_OC').click(function(event){
+    //filters out photos not in Oceania
+    $('.filter_OC').click(function(event) {
         event.preventDefault();
         $(this).parent().addClass('filter_active').siblings().removeClass('filter_active');
         console.log('OC Filter!');
@@ -156,10 +155,10 @@ function handleScroll(){
         $('.europe').fadeOut(500);
     })
 
-//filters out photos not in Asia
-       $('.filter_AZ').click(function(event){
+    //filters out photos not in Asia
+    $('.filter_AZ').click(function(event) {
         event.preventDefault();
-        $(this).parent().addClass('filter_active').siblings().removeClass('filter_active');    
+        $(this).parent().addClass('filter_active').siblings().removeClass('filter_active');
         console.log('AZ Filter!');
         $('.asia').fadeIn(500);
         $('.oceania').fadeOut(500);
